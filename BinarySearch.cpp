7ce0764baec658379c0f17d4d428479ff2f01a03
Ahad <ahadiqbal20@gmail.com>
2021-09-18 20:23:15 +0530

    Minor Error Sorted

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -8,8 +8,9 @@ using namespace std;
 int BinarySearch(int arr[],int n,int key){
     int start=0;
     int end=n;
-    int mid = (start+end)/2;
+    
     while(start<=end){
+        int mid = (start+end)/2;
         if(arr[mid]==key){
             return mid;     // We are returning the index at which that element will be found.
         }
